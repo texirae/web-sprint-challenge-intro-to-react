1fc7d7af30773ebdb29fe4bad3cefc3c9d90f544
Texi Schaeffer <texirae.schaeffer@gmail.com>
2022-11-29 14:38:54 -0600

    worked on character

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,6 +8,7 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
 
   return (
     <div className="App">
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+function Character(props){
+    const [character, setCharacter] = useState(props);
+
+    useEffect(()=> {
+        axios.get("https://swapi.dev/api/people/")
+            .then(res => {
+                setCharacter(res);
+            }).catch(err => console.error(err))
+    }, [])
+}
+
+  export default Character;
