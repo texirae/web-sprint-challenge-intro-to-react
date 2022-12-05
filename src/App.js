176505725cea12c9769b81b037a07e45801d6080
Texi Schaeffer <texirae.schaeffer@gmail.com>
2022-12-05 15:23:49 -0600

    completed styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import './App.css';
 import Character from './components/Character';
+import styled from 'styled-components';
+
+const StyledHeader = styled.h1`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+  text-align: center;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,6 +19,8 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+
   const [character, setCharacter] = useState([]);
   useEffect(()=> {
     axios.get("https://swapi.dev/api/people/")
@@ -20,7 +31,7 @@ const App = () => {
 }, [])
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <StyledHeader className="Header">Characters</StyledHeader>
       {character.map(ch => {
         return <Character name = {ch.name}/>
       })}
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,29 @@
 // Write your Character component here
-import React, {useEffect, useState} from "react";
+import React from "react";
+import styled from "styled-components";
 
 
 function Character(props){
 
+    const StyledContainer = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        color: #443e3e;
+        padding: 5px;
+        background: rgba(194, 178, 128, 0.6);
+        text-align: center;
+    `;
+
+    const StyledCharacter = styled.h2`
+        border-bottom: 5px solid #443e3e;
+    `;
+
     return(
-        <div className="character-container">
-            <h2>{props.name}</h2>
-        </div>
+        <StyledContainer className="character-container">
+            <StyledCharacter>{props.name}</StyledCharacter>
+        </StyledContainer>
     )
 }
 
